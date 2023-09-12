5594d4910097e14b9f88ca11b0e809ecadb6a229
Florescu Cristian <qflorescucristian@gmail.com>
2023-09-12 13:06:05 +0300

    Proxy design pattern

---
+++ proxy-pattern/Program.cs
@@ -0,0 +1,16 @@
+﻿using proxy_pattern;
+
+namespace Proxy.RealWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IMath math = new MathProxy();
+            Console.WriteLine("4 + 2 = " + math.Add(4, 2));
+            Console.WriteLine("4 - 2 = " + math.Sub(4, 2));
+            Console.WriteLine("4 * 2 = " + math.Mul(4, 2));
+            Console.WriteLine("4 / 2 = " + math.Div(4, 2));
+        }
+    }
+}
